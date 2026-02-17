01916def612f463856aceaee0debcde2a9335b03
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-18 01:36:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
 import requests
 
-'''
-'''
+themoviedb_API_KEY = os.environ.get("THEMOVIEDB_API_KEY")
 
-
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+
 
 class Base(DeclarativeBase):
     pass
@@ -47,19 +50,6 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-# with app.app_context():
-#
-#     db.session.add(second_movie)
-#     db.session.commit()
 
 with app.app_context():
     # db.session.add(new_movie)
@@ -72,19 +62,61 @@ def home():
     result = db.session.execute(db.select(Movie).order_by(Movie.ranking))
     all_movies = result.scalars().all()
 
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
 
     return render_template("index.html", movies=all_movies)
 
 
+#### --- Adding movies --- ####
+
+class MovieForm(FlaskForm):
+    title = StringField("Title")
+    submit = SubmitField("Add Movie")
+
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    form = MovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": themoviedb_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+@app.route("/find")
+def find():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        # The language parameter is optional, if you were making the website for a different audience
+        # e.g. Hindi speakers then you might choose "hi-IN"
+        response = requests.get(movie_api_url, params={"api_key": themoviedb_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            # The data in release_date includes month and day, we will want to get rid of.
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+
+##### ---  Edit   --- ####
 
 class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    ranking = StringField("Your ranking Out of 10")
     review = StringField("Your Review")
     submit = SubmitField("Submit")
 
-@app.route("/add")
-def add_movie():
-    pass
 
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
@@ -95,11 +127,22 @@ def edit():
     if form.validate_on_submit():
         movie.rating = float(form.rating.data)
         movie.review = form.review.data
+        movie.ranking = int(form.ranking.data)
 
         db.session.commit()
         return redirect(url_for("home"))
 
     return render_template("edit.html", movie=movie, form=form)
+
+
+
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)
