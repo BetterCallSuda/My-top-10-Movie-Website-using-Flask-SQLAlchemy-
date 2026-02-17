35f88ae5fb3d8b478fe357cd41818908428223ef
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-17 13:47:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from django.template.defaultfilters import title
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
-from torch import unique
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
@@ -12,27 +12,31 @@ import requests
 '''
 '''
 
-class Base(DeclarativeBase):
-    pass
 
-db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 Bootstrap5(app)
+
+class Base(DeclarativeBase):
+    pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 # CREATE DB
 class Movie(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    year = db.Column(db.Integer)
-    Description = db.Column(db.String(120))
-    rating = db.Column(db.Float)
-    ranking = db.Column(db.Float)
-    review = db.Column(db.String(120))
-    img_url = db.Column(db.String(120))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
 
     def __repr__(self):
         return (f"id= {self.title} title= {self.title} year= {self.year} rating= {self.rating}"
@@ -43,10 +47,29 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
+new_movie = Movie(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+
+with app.app_context():
+    # db.session.add(new_movie)
+    db.session.commit()
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    result = db.session.execute(db.select(Movie))
+    all_movies = result.scalars().all()
+
+
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
