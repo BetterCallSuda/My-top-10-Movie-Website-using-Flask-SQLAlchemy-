3b4b6368d3968fd42e5fd3db14ec0f90388b8fd4
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-17 23:36:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,5 +76,30 @@ def home():
     return render_template("index.html", movies=all_movies)
 
 
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Submit")
+
+@app.route("/add")
+def add_movie():
+    pass
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("edit.html", movie=movie, form=form)
+
 if __name__ == '__main__':
     app.run(debug=True)
